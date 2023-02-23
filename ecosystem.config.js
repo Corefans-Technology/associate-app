c015b7157412c3bab7cf10ea202a7eed41e51c6f
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2023-02-23 07:59:50 +0100

    adjust setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "corefans",
+      name: "corefans-manager",
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
